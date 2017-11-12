a397a52ababb24c807a5f18abf242b102ba0e04f
Noah Pryor <noah@noahpryor.com>
2017-11-12 15:21:20 -0500

    Update readme, add license, more cleanup

--- src/content-script.js
+++ src/content-script.js
@@ -1,22 +1,5 @@
 import { removeSharedConnections } from "./api";
 import arrive from "arrive"
-const selectors = {
-  // selectors for requests to connect
-  invitations: {
-    container: ".mn-invitation-card__invite-details-container",
-    details: ".mn-person-info__picture",
-    buttonContainer: ".mn-person-card__card-actions"
-  },
-  // selectors for people you may know
-  peopleYouMayKnow: {
-    container: ".mn-pymk-list__card",
-    details: ".mn-person-info__link",
-    buttonContainer: ".mn-person-info__card-details"
-  },
-  // contains count and names, only visible if you have mutual connections with someone
-  count: ".mn-person-info__shared-insights-count"
-};
-
 
 
 // Converts a linkedin profile page url or path into a linkedin shortname that can be used to query the api
@@ -73,9 +56,8 @@ function addRemoveAllButton(element, linkSelector, buttonLocation) {
 const addListenerToSelector = (personSelectors) => {
   document.arrive(".mn-person-info__shared-insights", function() {
       const summaryElement = this.closest(".ember-view")
-      addRemoveAllButton(summaryElement, personSelectors.details, ".mn-person-info__shared-insights")
+      addRemoveAllButton(summaryElement, ".mn-person-info__link", ".mn-person-info__shared-insights")
   })
 }
 
-// addListenerToSelector(selectors.invitations)
-addListenerToSelector(selectors.peopleYouMayKnow)
+addListenerToSelector()
